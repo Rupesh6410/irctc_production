ff676c3ddbf796e4a73ece8de91d4363429cae27
Rupesh <Rupeshkumar6410@gmail.com>
2026-05-16 14:02:41 +0530

    setup

---
+++ user-service/src/config/index.js
@@ -0,0 +1,13 @@
+const config = {
+  SERVICE_NAME: require('../../package.json').name,
+  PORT: Number(process.env.PORT) || 4001,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  LOG_LEVEL: process.env.LOG_LEVEL || "info",
+  DATABASE_URL: process.env.DATABASE_URL,
+  REDIS_URL: process.env.REDIS_URL,
+  KAFKA_BROKER: process.env.KAFKA_BROKER,
+  KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID,
+  ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
+
+}
+module.exports = { config };
